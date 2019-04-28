6fba40a5e6e918314f235041217952d16665c92d
facundonapal <indiecitus@gmail.com>
2019-04-28 18:31:30 +0200

    first commit

---
+++ js/navbarTab.js
@@ -0,0 +1,5 @@
+element1 = document.createElement('li');
+element1.appendChild(document.createTextNode('<a href="lookbook.html">#LiveLoveDubarry</a>'));
+element1.class = 'top has-children pages';
+element2 = document.getElementById('main-menu');
+element2.appendChild(element1);
